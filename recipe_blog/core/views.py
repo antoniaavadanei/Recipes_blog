d383f55f79448af9e3c4df68a644fed89272a618
antoniaavadanei <antoavadanei@yahoo.com>
2022-12-07 00:17:06 +0200

    add_post

---
+++ recipe_blog/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+
+
+# def home (request):
+#     return render(request,'home.html',{})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class CreatePostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+    #fields = ('title','author','body')
