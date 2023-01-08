084eeaac6c210cf14d8facc435ea3acc05f07d44
antoniaavadanei <antoavadanei@yahoo.com>
2023-01-08 02:26:35 +0200

    Delete post and order posts by last created

--- recipe_blog/core/urls.py
+++ recipe_blog/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView,PostDetailView, CreatePostView, UpdatePostView
+from .views import HomeView,PostDetailView, CreatePostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     # path('',views.home, name="home"),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('post/<int:pk>',PostDetailView.as_view(),name="post_detail"),
     path('add_post/',CreatePostView.as_view(),name="add_post"),
     path('post/edit/<int:pk>',UpdatePostView.as_view(),name="update_post"),
+    path('post/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
 ]
 
