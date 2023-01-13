e8ccc9d8af9da2bc88bf2c74fa8a6c1bedf91f0d
antoniaavadanei <antoavadanei@yahoo.com>
2023-01-13 22:58:41 +0200

    Add Category

--- recipe_blog/core/models.py
+++ recipe_blog/core/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from datetime import datetime,date
+from datetime import datetime, date
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -10,10 +21,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default="Desert")
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
-        #return reverse('post_detail', args=(str(self.id)))
+        # return reverse('post_detail', args=(str(self.id)))
         return reverse('home')
--- recipe_blog/core/urls.py
+++ recipe_blog/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView,PostDetailView, CreatePostView, UpdatePostView, DeletePostView
+from .views import HomeView,PostDetailView, CreatePostView, UpdatePostView, DeletePostView, CreateCategoryView
 
 urlpatterns = [
     # path('',views.home, name="home"),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('add_post/',CreatePostView.as_view(),name="add_post"),
     path('post/edit/<int:pk>',UpdatePostView.as_view(),name="update_post"),
     path('post/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
+    path('add_category/',CreateCategoryView.as_view(),name="add_category")
 ]
 
